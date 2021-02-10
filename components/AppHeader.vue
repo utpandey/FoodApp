940229017cd1ac15a46eae3db741a7e7e198bec4
utpandey <utsavpandey8@gmail.com>
2021-02-10 10:47:41 +0530

    home page button fixed #1

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="masthead" role="img" aria-label="Image Description">
     <h1>Nuxt Food App</h1>
-    <button>See Restaurants</button>
+    <nuxt-link to="/restaurants"
+      ><button>View our Restaurant</button></nuxt-link
+    >
   </section>
 </template>
 
